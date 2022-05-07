e46d60aa450317191800385bcbbc80dc6f0af4b7
priti401 <pitimaurya401@gamil.com>
2022-05-07 16:17:22 +0530

    changessss

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../services/login.service';
+
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { TokenService } from '../services/token.service';
-import { ToastrService } from 'ngx-toastr';
+// import { TokenService } from '../services/token.service';
+// import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,15 +14,16 @@ export class LoginComponent implements OnInit {
   LoginForm!: FormGroup;
   isSubmitted  =  false;
  
-  constructor( private loginService: LoginService,
-    private tokenService: TokenService,
-    private toastr: ToastrService,
+  constructor( 
+    
     private fb: FormBuilder,
-    private router: Router) { }
+    ) { }
 
   ngOnInit(): void {
+  
+ 
     this. LoginForm  =  this.fb.group({
-      context: ['Web',Validators.required],
+      context: ['',Validators.required],
       Username: ['', Validators.required],
       password: ['', Validators.required]
   });
