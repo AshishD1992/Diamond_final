c7cf6919c6dde96819b6a80d2817733bcecc4f2b
priti401 <pitimaurya401@gamil.com>
2022-05-10 10:56:37 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-// import { TokenService } from '../services/token.service';
-// import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -11,33 +10,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  LoginForm!: FormGroup;
-  isSubmitted  =  false;
- 
-  constructor( 
-    
-    private fb: FormBuilder,
-    ) { }
-
-  ngOnInit(): void {
+  LoginForm:any = FormGroup;
+submitted = false;
+constructor( private formBuilder: FormBuilder,
+ ){}
+//Add user form actions
+get f() { return this.LoginForm.controls; }
+onSubmit() {
   
- 
-    this. LoginForm  =  this.fb.group({
-      context: ['',Validators.required],
-      Username: ['', Validators.required],
-      password: ['', Validators.required]
-  });
-  }
-  get f() 
-  { return this. LoginForm.controls; }
-
-  login(){
-    console.log(this. LoginForm.value);
-    this.isSubmitted = true;
-    if(this. LoginForm.invalid){
+  this.submitted = true;
+  // stop here if form is invalid
+  if (this.LoginForm.invalid) {
       return;
-    }
-    // this.authService.login(this.loginForm.value);
-    // this.router.navigateByUrl('/admin');
+  }
+
+  
+  //True if all the fields are filled
+  if(this.submitted)
+  {
+    alert("Great!!");
+  }
+ 
+}
+  ngOnInit() {
+    //Add User form validations
+    this.LoginForm = this.formBuilder.group({
+    user: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+    });
   }
 }
